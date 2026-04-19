1e72b4aea4a6872360e33a3b980d20346b29ffcd
thatraghavarora <raghavarora930@gmail.com>
2026-04-19 13:04:25 +0530

    push

--- src/app/components/LandingScreen.tsx
+++ src/app/components/LandingScreen.tsx
@@ -23,7 +23,7 @@ export function LandingScreen({ onNext, onSocialLogin }: LandingScreenProps) {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin
+          redirectTo: 'https://peerup-students.vercel.app/'
         }
       });
 
